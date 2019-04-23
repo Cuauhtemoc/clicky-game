e3fe70287548c6e2e9586050c1ef86609790d682
Cuauhtemoc Rodriguez <crodri606@gmail.com>
2019-04-23 17:28:04 -0500

    Updates

---
+++ precache-manifest.e16fd31a5922385413b09e54cc653765.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "223b4f588b22072dab3a",
+    "url": "/Cuauhtemoc/clicky-game/static/js/runtime~main.4ebf0361.js"
+  },
+  {
+    "revision": "13b4e9cc940d61866beb",
+    "url": "/Cuauhtemoc/clicky-game/static/js/main.5b7ceac0.chunk.js"
+  },
+  {
+    "revision": "d82afacfb8046780bdff",
+    "url": "/Cuauhtemoc/clicky-game/static/js/2.c272f57a.chunk.js"
+  },
+  {
+    "revision": "13b4e9cc940d61866beb",
+    "url": "/Cuauhtemoc/clicky-game/static/css/main.3eab6afd.chunk.css"
+  },
+  {
+    "revision": "6fe88f6207ca8e97db92b01fe7d25c9b",
+    "url": "/Cuauhtemoc/clicky-game/index.html"
+  }
+];
